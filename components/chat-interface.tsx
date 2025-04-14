0efb1b82dc6f7f48e45d0cd6a1858b622388c5c6
Mark kibocha <mark.varu@student.cuk.ac.ke>
2025-04-14 11:40:04 +0300

    Update chat-interface.tsx

--- components/chat-interface.tsx
+++ components/chat-interface.tsx
@@ -61,7 +61,7 @@ export function ChatInterface({ history }: {
   const [optimisticMessages, addOptimisticMessage] = useOptimistic<ChatStatus[], ChatMessage>(
     history,
     (prevState, newMessage) => {
-      console.log(newMessage, "ne")
+     
       const newEntry: ChatStatus = {
         status: 200,
         history: [newMessage],
@@ -150,15 +150,15 @@ export function ChatInterface({ history }: {
     onSubmit(values) {
 
       formik.setSubmitting(false)
-      startTransition(() => {
-        addOptimisticMessage({
-          id: (optimisticMessages[1] as any).length + 1,
-          query: formik.values.query,
-          response: "",
-          created_at: new Date(),
-          updated_at: new Date(),
-        });
-      });
+      // startTransition(() => {
+      //   addOptimisticMessage({
+      //     id: (optimisticMessages[1] as any).length + 1,
+      //     query: formik.values.query,
+      //     response: "",
+      //     created_at: new Date(),
+      //     updated_at: new Date(),
+      //   });
+      // });
       
       querymutation.mutateAsync(values.query)
 
